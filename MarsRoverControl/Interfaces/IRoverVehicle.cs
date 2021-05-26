e2431434feac1d33b7bda319855f81f6e70cb2b2
Okan YÜKSEL <okannnyuksel@gmail.com>
2021-05-26 22:11:32 +0300

    RoverVehicle object class, IRoverVehicle interface and direction enum generated

---
+++ MarsRoverControl/Interfaces/IRoverVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverControl.Interfaces
+{
+    public interface IRoverVehicle
+    {
+        public string TurnLeft();
+        public string TurnRight();
+        public string MoveForward();
+        public int GetDirectionCount();
+        public string GetRoverPositionAndDirection();
+        public string GetRoverPositionOnSurface();
+    }
+}
